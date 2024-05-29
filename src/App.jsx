b7ddb49c22ccede5d0332aba7b0a8790316f5b2c
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-05-28 19:23:02 -0500

    add basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch.jsx';
+import { TodoList } from './TodoList.jsx';
+import { TodoItem } from './TodoItem.jsx';
+import { CreateTodoButton } from './CreateTodoButton';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoCounter total={25} completed={16} />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+        <CreateTodoButton/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
